5372743d7c5e697f6c09713238cd69c8ea879133
Phuong <phuongngovan2003@gmail.com>
2024-03-26 12:42:15 +0700

    Add React navigation package

--- client/App.js
+++ client/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TestApp from './components/TestApp';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
-      <TestApp />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
