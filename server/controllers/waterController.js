13e7986728361ad17123dd420350457d73c9dd22
PhucTran <tranbaophuc2703@gmail.com>
2024-03-28 15:03:24 +0700

    update feed

--- server/controllers/waterController.js
+++ server/controllers/waterController.js
@@ -4,8 +4,22 @@ let status = "auto";
 
 if (status == "auto") {
   get_humi((valueLoad) => {
-    console.log("Humi Value:", valueLoad);
+    console.log("Soil Moisture:", valueLoad);
     if (valueLoad < 60) act_pump();
     if (valueLoad > 80) inact_pump();
   });
+}
+
+function changeMode(req, res, next){
+    try{
+        mode=req.body.mode;
+        res.json("complete");
+    }
+    catch(err){
+        next(err);
+    }
+}
+
+module.exports = {
+    changeMode
 }
