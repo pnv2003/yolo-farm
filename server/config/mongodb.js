3caea4afb5d0ca795b6e467cc2d2d04ef51cd6b8
Huỳnh Nguyên Phúc <phucthcsmyloc@gmail.com>
2024-04-01 22:23:12 +0700

    Fix MongoDB connection

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -3,20 +3,26 @@ const { MongoClient} = require("mongodb");
 
 const MONGODB_URI =
   process.env.MONGODB_URI ||
-  "mongodb+srv://phuctran2703:RSqZV9ufQ1pBaxwW@cluster0.krbalrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://phuchuynh0904:09042003@cluster0.krbalrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // connect MongoDB
-const client = new MongoClient(MONGODB_URI);
+const client = new MongoClient(MONGODB_URI, {
+  maxPoolSize: 10
+});
 
-// connect database
-var database;
-try {
-  client.connect();
-  console.log("Connected to MongoDB");
-  database = client.db("DADN-AI");
-  console.log("Connected to Database DADN-AI");
-} catch (error) {
-  console.error("Error connecting to MongoDB:", error);
+let database;
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    database = client.db("YOLO Farm");
+    console.log("Connected to Database DADN-AI");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 }
+
+connectToDatabase();
 
 module.exports = database;
