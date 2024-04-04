fdfdb572e5096565ab978ac953bfc17fa148bda5
PhucTran <tranbaophuc2703@gmail.com>
2024-04-04 10:03:21 +0700

    update link

--- server/routes/watering.js
+++ server/routes/watering.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const waterController = require('../controllers/waterController');
 
 
-router.get('/get-moisture', waterController.getMoisture);
+router.get('/moisture', waterController.getMoisture);
 
-router.post('/post-moisture', waterController.setMoisture);
+router.post('/moisture', waterController.setMoisture);
 
-router.get('/get-mode', waterController.getMode);
+router.get('/mode', waterController.getMode);
 
-router.post('/post-mode', waterController.setMode);
+router.post('/mode', waterController.setMode);
 
-router.get('/get-min-moisture', waterController.getMinMoisture);
+router.get('/min-moisture', waterController.getMinMoisture);
 
-router.post('/post-min-moisture', waterController.setMinMoisture);
+router.post('/min-moisture', waterController.setMinMoisture);
 
-router.get('/get-max-moisture', waterController.getMaxMoisture);
+router.get('/max-moisture', waterController.getMaxMoisture);
 
-router.post('/post-max-moisture', waterController.setMaxMoisture);
+router.post('/max-moisture', waterController.setMaxMoisture);
 
 
 module.exports = router;
