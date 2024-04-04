91d88da93e26035a5850c2a5fa3b8257710903ff
PhucTran <tranbaophuc2703@gmail.com>
2024-04-04 10:23:29 +0700

    :))

--- server/routes/watering.js
+++ server/routes/watering.js
@@ -5,15 +5,15 @@ const waterController = require('../controllers/waterController');
 
 router.get('/moisture', waterController.getMoisture);
 
-router.post('/moisture', waterController.setMoisture);
+router.put('/moisture', waterController.setMoisture);
 
 router.get('/mode', waterController.getMode);
 
-router.post('/mode', waterController.setMode);
+router.put('/mode', waterController.setMode);
 
 router.get('/min-max-moisture', waterController.getMinMaxMoisture);
 
-router.post('/min-max-moisture', waterController.setMinMaxMoisture);
+router.put('/min-max-moisture', waterController.setMinMaxMoisture);
 
 
 module.exports = router;
