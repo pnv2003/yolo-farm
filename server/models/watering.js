8143171abac02310b96844df1973986ed894955f
PhucTran <tranbaophuc2703@gmail.com>
2024-04-04 11:08:55 +0700

    update

--- server/models/watering.js
+++ server/models/watering.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-var mode = "Automatic"; //Automatic or Manual
+var mode = "automatic"; //Automatic or Manual
 var soil_moisture = 0; // cần lấy giá trị mới nhất trong database
 var min_moisture = 40;
 var max_moisture = 60;
@@ -14,7 +14,7 @@ async function setMoisture(value) {
 }
 
 async function getMode() {
-  return { Mode: mode };
+  return { mode: mode };
 }
 
 async function setMode(value) {
@@ -32,13 +32,13 @@ async function set_minmax_moisture(value1, value2) {
 
 async function checkMoisture(value) {
   if (value < min_moisture) {
-    if (mode == "Automatic") {
+    if (mode == "automatic") {
       await act_pump();
     } else {
       console.log("Warning: Soil Moisture is low");
     }
   } else if (value > max_moisture) {
-    if (mode == "Automatic") {
+    if (mode == "automatic") {
       await inact_pump();
     } else {
       console.log("Warning: Soil Moisture is high");
