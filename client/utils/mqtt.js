b3cbe8f30b450850206bdbd0525d1bce9a393235
Phuong <phuongngovan2003@gmail.com>
2024-04-05 01:02:08 +0700

    Modularize MQTT connection

---
+++ client/utils/mqtt.js
@@ -0,0 +1,44 @@
+import Paho from 'paho-mqtt';
+import { AIO_HOST, AIO_PATH, AIO_PORT, mqttClientID } from '../config/connect';
+import { AIO_KEY, AIO_USERNAME } from '../config/account';
+
+export function init() {
+    return new Paho.Client(
+        AIO_HOST,
+        AIO_PORT,
+        AIO_PATH,
+        mqttClientID()
+    );
+}
+
+export function connect(client, feeds) {
+    client.connect({
+        onSuccess: () => {
+            console.log("MQTT connection success");
+            feeds.forEach((feed) => {
+                client.subscribe(feed);
+            });
+        },
+        onFailure: (error) => {
+            console.log("MQTT connection failed");
+            console.log(error.errorMessage);
+        },
+        userName: AIO_USERNAME,
+        password: AIO_KEY
+    })
+}
+
+export function publish(client, topic, message) {
+    let msg = new Paho.Message(message);
+    msg.destinationName = topic;
+    client.send(msg);
+}
+
+// export function setOnMessageArrived(client, onMessageArrived) {
+//     client.onMessageArrived = onMessageArrived;
+// }
+
+export function disconnect(client) {
+    console.log("MQTT disconnected");
+    client.disconnect();
+}
