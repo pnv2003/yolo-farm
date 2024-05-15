a0b8baea76c538b2a25f35d1ee4b689258770fb8
PhucTran <tranbaophuc2703@gmail.com>
2024-05-15 13:02:07 +0700

    update disease detection

---
+++ server/models/leaf-disease-detection.js
@@ -0,0 +1,27 @@
+var spawn = require("child_process").spawn;
+var process = spawn("python", ["../AI-Model/diseaseDetection/main.py"]);
+
+function getResult() {
+    return new Promise((resolve, reject) => {
+      const process = spawn("python", ["AI-Model/diseaseDetection/main.py"]);
+  
+      let result = "";
+  
+      process.stdout.on("data", function (data) {
+        result += data.toString();
+      });
+  
+      process.on("close", function (code) {
+        if (code !== 0) {
+          reject(new Error(`Process exited with code ${code}`));
+          return;
+        }
+        resolve(result);
+      });
+    });
+  }
+  
+
+module.exports = {
+  getResult,
+};
