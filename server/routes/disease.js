ff424f33f156cacbc7531a5d1269934e2213b9c4
PhucTran <tranbaophuc2703@gmail.com>
2024-05-16 21:30:46 +0700

    update disease

--- server/routes/disease.js
+++ server/routes/disease.js
@@ -2,7 +2,26 @@ const express = require('express');
 const router = express.Router();
 const diseaseDetection = require('../controllers/diseaseDetection');
 
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-router.get('/', diseaseDetection.getDisease);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      const dir = './AI-Model/diseaseDetection/picture';
+      if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+      }
+      cb(null, dir);
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
+  
+  // Initialize upload variable
+  const upload = multer({ storage: storage });
+
+router.get('/', upload.single('image'), diseaseDetection.getDisease);
 
 module.exports = router;
